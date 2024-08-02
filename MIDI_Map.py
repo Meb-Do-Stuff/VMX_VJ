403f6b3b13ccadb0ae99ec4c1f860935efe005e1
Meb <meblikea@gmail.com>
2024-08-02 13:11:23 +0200

    Preparing for mapping

--- MIDI_Map.py
+++ MIDI_Map.py
@@ -19,25 +19,25 @@ MESSAGETYPE = 0  # Message type for buttons/pads; set to 0 for MIDI Notes, 1 for
 
 
 # Track selection box (aka that coloured box for scene/track launching)
-TSB_X = 8   # Controls the vertical value for the track selection box. Default value is 8
-TSB_Y = 8   # Controls the horizontal value for the track selection box. Default value is 8
+TSB_X = 16  # Controls the vertical value for the track selection box. Default value is 8
+TSB_Y = 4  # Controls the horizontal value for the track selection box. Default value is 8
 
 # General
-PLAY = -1   # Global play
-STOP = -1   # Global stop
-REC = -1    # Global record
-TAPTEMPO = -1   # Tap tempo
-NUDGEUP = -1    # Tempo Nudge Up
+PLAY = -1  # Global play
+STOP = -1  # Global stop
+REC = -1  # Global record
+TAPTEMPO = -1  # Tap tempo
+NUDGEUP = -1  # Tempo Nudge Up
 NUDGEDOWN = -1  # Tempo Nudge Down
-UNDO = -1   # Undo
-REDO = -1   # Redo
-LOOP = -1   # Loop on/off
-PUNCHIN = -1    # Punch in
-PUNCHOUT = -1   # Punch out
-OVERDUB = -1     # Overdub on/off
-METRONOME = -1   # Metronome on/off
-RECQUANT = -1   # Record quantization on/off
-DETAILVIEW = -1   # Detail view switch
+UNDO = -1  # Undo
+REDO = -1  # Redo
+LOOP = -1  # Loop on/off
+PUNCHIN = -1  # Punch in
+PUNCHOUT = -1  # Punch out
+OVERDUB = -1  # Overdub on/off
+METRONOME = -1  # Metronome on/off
+RECQUANT = -1  # Record quantization on/off
+DETAILVIEW = -1  # Detail view switch
 CLIPTRACKVIEW = -1  # Clip/Track view switch
 
 # Device Control
@@ -47,7 +47,7 @@ DEVICENAVLEFT = -1  # Device nav left
 DEVICENAVRIGHT = -1  # Device nav right
 DEVICEBANKNAVLEFT = -1  # Device bank nav left
 DEVICEBANKNAVRIGHT = -1  # Device bank nav right
-DEVICEBANK = (-1,  # Bank 1 #All 8 banks must be assigned to positive values in order for bank selection to work
+DEVICEBANK = (-1,  # Bank 1
               -1,  # Bank 2
               -1,  # Bank 3
               -1,  # Bank 4
@@ -85,10 +85,6 @@ SCENELAUNCH = (-1,  # Scene 1 Launch
                -1,  # Scene 2
                -1,  # Scene 3
                -1,  # Scene 4
-               -1,  # Scene 5
-               -1,  # Scene 6
-               -1,  # Scene 7
-               -1,  # Scene 8
                )
 
 # Clip Launch / Stop
@@ -96,22 +92,18 @@ SELCLIPLAUNCH = -1  # Selected clip launch
 STOPALLCLIPS = -1  # Stop all clips
 
 # 8x8 Matrix note assignments
-# Track no.:     1   2   3   4   5   6   7   8
-CLIPNOTEMAP = ((-1, -1, -1, -1, -1, -1, -1, -1),  # Row 1
-               (-1, -1, -1, -1, -1, -1, -1, -1),  # Row 2
-               (-1, -1, -1, -1, -1, -1, -1, -1),  # Row 3
-               (-1, -1, -1, -1, -1, -1, -1, -1),  # Row 4
-               (-1, -1, -1, -1, -1, -1, -1, -1),  # Row 5
-               (-1, -1, -1, -1, -1, -1, -1, -1),  # Row 6
-               (-1, -1, -1, -1, -1, -1, -1, -1),  # Row 7
-               (-1, -1, -1, -1, -1, -1, -1, -1),  # Row 8
+# Track no.:     1   2   3   4   5   6   7   8   9   10   11   12   13   14   15   16
+CLIPNOTEMAP = ((-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1),  # Row 1
+               (-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1),  # Row 2
+               (-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1),  # Row 3
+               (-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1),  # Row 4
                )
 
 # Track Control
-MASTERSEL = -1      # Master track select
-SELTRACKREC = -1    # Arm Selected Track
-SELTRACKSOLO = -1   # Solo Selected Track
-SELTRACKMUTE = -1   # Mute Selected Track
+MASTERSEL = -1  # Master track select
+SELTRACKREC = -1  # Arm Selected Track
+SELTRACKSOLO = -1  # Solo Selected Track
+SELTRACKMUTE = -1  # Mute Selected Track
 
 TRACKSTOP = (-1,  # Track 1 Clip Stop
              -1,  # Track 2
@@ -121,6 +113,14 @@ TRACKSTOP = (-1,  # Track 1 Clip Stop
              -1,  # Track 6
              -1,  # Track 7
              -1,  # Track 8
+             -1,  # Track 9
+             -1,  # Track 10
+             -1,  # Track 11
+             -1,  # Track 12
+             -1,  # Track 13
+             -1,  # Track 14
+             -1,  # Track 15
+             -1,  # Track 16
              )
 
 TRACKSEL = (-1,  # Track 1 Select
@@ -131,6 +131,14 @@ TRACKSEL = (-1,  # Track 1 Select
             -1,  # Track 6
             -1,  # Track 7
             -1,  # Track 8
+            -1,  # Track 9
+            -1,  # Track 10
+            -1,  # Track 11
+            -1,  # Track 12
+            -1,  # Track 13
+            -1,  # Track 14
+            -1,  # Track 15
+            -1,  # Track 16
             )
 
 TRACKMUTE = (-1,  # Track 1 On/Off
@@ -141,6 +149,14 @@ TRACKMUTE = (-1,  # Track 1 On/Off
              -1,  # Track 6
              -1,  # Track 7
              -1,  # Track 8
+             -1,  # Track 9
+             -1,  # Track 10
+             -1,  # Track 11
+             -1,  # Track 12
+             -1,  # Track 13
+             -1,  # Track 14
+             -1,  # Track 15
+             -1,  # Track 16
              )
 
 TRACKSOLO = (-1,  # Track 1 Solo
@@ -151,6 +167,14 @@ TRACKSOLO = (-1,  # Track 1 Solo
              -1,  # Track 6
              -1,  # Track 7
              -1,  # Track 8
+             -1,  # Track 9
+             -1,  # Track 10
+             -1,  # Track 11
+             -1,  # Track 12
+             -1,  # Track 13
+             -1,  # Track 14
+             -1,  # Track 15
+             -1,  # Track 16
              )
 
 TRACKREC = (-1,  # Track 1 Record
@@ -161,8 +185,15 @@ TRACKREC = (-1,  # Track 1 Record
             -1,  # Track 6
             -1,  # Track 7
             -1,  # Track 8
+            -1,  # Track 9
+            -1,  # Track 10
+            -1,  # Track 11
+            -1,  # Track 12
+            -1,  # Track 13
+            -1,  # Track 14
+            -1,  # Track 15
+            -1,  # Track 16
             )
-
 
 # Pad Translations for Drum Rack
 PADCHANNEL = 0  # MIDI channel for Drum Rack notes
@@ -171,7 +202,6 @@ DRUM_PADS = (-1, -1, -1, -1,  # MIDI note numbers for 4 x 4 Drum Rack
              -1, -1, -1, -1,  # Notes will be "swallowed" if already mapped elsewhere
              -1, -1, -1, -1,
              )
-
 
 # Sliders / Knobs
 # ---------------
@@ -194,6 +224,14 @@ TRACKVOL = (-1,  # Track 1 Volume
             -1,  # Track 6
             -1,  # Track 7
             -1,  # Track 8
+            -1,  # Track 9
+            -1,  # Track 10
+            -1,  # Track 11
+            -1,  # Track 12
+            -1,  # Track 13
+            -1,  # Track 14
+            -1,  # Track 15
+            -1,  # Track 16
             )
 
 TRACKPAN = (-1,  # Track 1 Pan
@@ -204,6 +242,14 @@ TRACKPAN = (-1,  # Track 1 Pan
             -1,  # Track 6
             -1,  # Track 7
             -1,  # Track 8
+            -1,  # Track 9
+            -1,  # Track 10
+            -1,  # Track 11
+            -1,  # Track 12
+            -1,  # Track 13
+            -1,  # Track 14
+            -1,  # Track 15
+            -1,  # Track 16
             )
 
 TRACKSENDA = (-1,  # Track 1 Send A
@@ -214,6 +260,14 @@ TRACKSENDA = (-1,  # Track 1 Send A
               -1,  # Track 6
               -1,  # Track 7
               -1,  # Track 8
+              -1,  # Track 9
+              -1,  # Track 10
+              -1,  # Track 11
+              -1,  # Track 12
+              -1,  # Track 13
+              -1,  # Track 14
+              -1,  # Track 15
+              -1,  # Track 16
               )
 
 TRACKSENDB = (-1,  # Track 1 Send B
@@ -224,6 +278,14 @@ TRACKSENDB = (-1,  # Track 1 Send B
               -1,  # Track 6
               -1,  # Track 7
               -1,  # Track 8
+              -1,  # Track 9
+              -1,  # Track 10
+              -1,  # Track 11
+              -1,  # Track 12
+              -1,  # Track 13
+              -1,  # Track 14
+              -1,  # Track 15
+              -1,  # Track 16
               )
 
 TRACKSENDC = (-1,  # Track 1 Send C
@@ -234,6 +296,14 @@ TRACKSENDC = (-1,  # Track 1 Send C
               -1,  # Track 6
               -1,  # Track 7
               -1,  # Track 8
+              -1,  # Track 9
+              -1,  # Track 10
+              -1,  # Track 11
+              -1,  # Track 12
+              -1,  # Track 13
+              -1,  # Track 14
+              -1,  # Track 15
+              -1,  # Track 16
               )
 
 PARAMCONTROL = (-1,  # Param 1 #All 8 params must be assigned to positive values in order for param control to work
@@ -245,5 +315,3 @@ PARAMCONTROL = (-1,  # Param 1 #All 8 params must be assigned to positive values
                 -1,  # Param 7
                 -1,  # Param 8
                 )
-
-
