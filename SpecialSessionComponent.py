72e3b16b8feb2d3d30862103bec1434c54816462
Meb do stuff <69876322+Meb-Do-Stuff@users.noreply.github.com>
2024-08-08 02:26:42 +0200

    Removed unused value & optimized the code Managed to bind every buttons to delete clips when a button is pressed (learnt intreaction between _Framework and ableton modules) Still have to: Finish the deletion system (delete clip or default selected stuff) Optimize stuff (trying to have menu managers and everything in VMX_V64.py and Custom Clip Shot file) (For the buttons binding, have to find how to unbind)

--- SpecialSessionComponent.py
+++ SpecialSessionComponent.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import Live
+from ableton.v3.base import compose, find_if
 from _Framework.SessionComponent import SessionComponent
 from _Framework.ButtonElement import ButtonElement
 from math import sqrt
@@ -19,6 +20,7 @@ class SpecialSessionComponent(SessionComponent):
         self._alt0_igniter = buttons[int(sqrt(len(buttons))) * -1]
         self._alt1_igniter = buttons[int(sqrt(len(buttons))) * -1 + 1]
         # self._igniter.is_momentary = False
+        self._deletion_bind_functions = [[]] * 4
         self._buttons = buttons
         self._mixer = mixer
         self._transport = transport
@@ -36,14 +38,11 @@ class SpecialSessionComponent(SessionComponent):
         for scene_index in range(self.num_scenes):
             scene = self.scene(scene_index)
             scene.name = 'Scene_' + str(scene_index)
-            button_row = []
             scene.set_triggered_value(2)
             for track_index in range(self.num_tracks):
-                button = self.clip_launch_buttons[scene_index][track_index]
-                button_row.append(button)
                 clip_slot = scene.clip_slot(track_index)
                 clip_slot.name = str(track_index) + '_Clip_Slot_' + str(scene_index)
-                clip_slot.set_launch_button(button)
+                clip_slot.set_launch_button(self.clip_launch_buttons[scene_index][track_index])
 
     def unbind_clip_launch(self):
         for scene_index in range(self.num_scenes):
@@ -53,19 +52,27 @@ class SpecialSessionComponent(SessionComponent):
                 clip_slot.set_launch_button(None)
 
     def setup_clip_delete(self):
-        self.unbind_clip_launch()
         for scene_index in range(self.num_scenes):
             scene = self.scene(scene_index)
             scene.name = 'Scene_' + str(scene_index)
-            button_row = []
             scene.set_triggered_value(2)
             for track_index in range(self.num_tracks):
                 button = self.clip_launch_buttons[scene_index][track_index]
-                button_row.append(button)
                 clip_slot = scene.clip_slot(track_index)
                 clip_slot.name = str(track_index) + '_Clip_Slot_' + str(scene_index)
-                clip_slot.set_delete_button(button)
-                # clip_slot.set_launch_button(button)
+                deletion_lambda = lambda value, cs=clip_slot: self._delete_clip(cs, value)
+                button.add_value_listener(deletion_lambda)
+                self._deletion_bind_functions[scene_index].append(deletion_lambda)
+
+    def unbind_clip_delete(self):
+        for scene_index in range(self.num_scenes):
+            for track_index in range(self.num_tracks):
+                self.clip_launch_buttons[scene_index][track_index].remove_value_listener(self._deletion_bind_functions[scene_index][track_index])
+        self._deletion_bind_functions = [[]] * 4
+
+    def _delete_clip(self, clip_slot, value):
+        if clip_slot._clip_slot is not None and clip_slot._clip_slot.has_clip:
+            clip_slot._clip_slot.delete_clip()
 
     def disconnect(self):
         SessionComponent.disconnect(self)
@@ -122,9 +129,11 @@ class SpecialSessionComponent(SessionComponent):
             for scene_index in range(self.num_scenes - 1):
                 scene = self.scene(scene_index)
                 scene.name = 'Scene_' + str(scene_index)
-                scene.set_launch_button(self.clip_launch_buttons[scene_index][-1])  # Button is in push mode while it's toggle (problem comes from scene scripts (have to figure out a way to bypass the problem))
+                scene.set_launch_button(self.clip_launch_buttons[scene_index][
+                                            -1])  # Button is in push mode while it's toggle (problem comes from scene scripts (have to figure out a way to bypass the problem))
                 scene.set_triggered_value(2)
-            self.set_stop_track_clip_buttons([self.clip_launch_buttons[-1][track_index] for track_index in range(self.num_tracks-1)])
+            self.set_stop_track_clip_buttons(
+                [self.clip_launch_buttons[-1][track_index] for track_index in range(self.num_tracks - 1)])
         elif value == 0:
             for scene_index in range(self.num_scenes):
                 self.scene(scene_index).set_launch_button(None)
@@ -153,8 +162,8 @@ class SpecialSessionComponent(SessionComponent):
 
     def _deletion(self, value):
         if value == 127 and not self._alt0_igniter.is_pressed() and not self._alt1_igniter.is_pressed():
-            Live.Base.log("Delete button pressed")
+            self.unbind_clip_launch()
             self.setup_clip_delete()
         if value == 0:
-            Live.Base.log("Delete button released")
+            self.unbind_clip_delete()
             self.setup_clip_launch()
